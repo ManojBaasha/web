06b42f75d9a7bb5dacb4b3a3a24d3e644ce16f84
ManojBaasha <66433730+ManojBaasha@users.noreply.github.com>
2025-03-27 02:54:04 +0000

    Deploying to gh-pages from @ ManojBaasha/web@1fd3671b46c2236c2e1d80fb305771915a1be6dc 🚀

---
+++ _app/immutable/chunks/stores.808fa3ec.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.61d7efd7.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
